25ec4f589def25519bd7bff6b981f3008474f690
Mandavaganesh <153338985+Mandavaganesh@users.noreply.github.com>
2024-04-27 06:26:41 +0530

    Update Submit.js

--- frontend/src/components/Submit.js
+++ frontend/src/components/Submit.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 const Submit = () => {
   return (
-    <div><h1><center>Thanks for Submitted</center></h1></div>
-  )
-}
+    <div style={{ 
+      backgroundImage: "url('https://cdn.dribbble.com/users/273990/screenshots/5519988/thanks-for-voting.jpg')",
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      height: '95vh',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      }}></div>
+  );
+};
 
 export default Submit;
